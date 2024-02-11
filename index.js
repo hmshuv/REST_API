50c96822aa5e986ee33c822358dd5f17a11eb916
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-12 00:51:26 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const users = require('./MOCK_DATA.json');
+const users = require("./MOCK_DATA.json");
 const app = express();
 
 const PORT = 5050;
 
 
 //ROUTES
-app.get('/users', (req, res) =>{
+app.get("/", (req, res)=>{
+    res.send("Welcome to home page of REST API project");
+})
+app.get("/users", (req, res) =>{
     return res.json(users);
-} );
-express().listen(PORT, ()=> console.log(`server listening to port ${PORT}`));
+})
+
+app.listen(PORT, ()=> console.log(`server listening to port ${PORT}`));
