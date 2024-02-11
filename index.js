472bb9615a5128d89b61d218dbd334bcacbe3557
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-12 01:12:51 +0530

    commit

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const PORT = 5050;
 app.get("/", (req, res)=>{
     return res.send("Welcome to home page of REST API project");
 })
-app.get("/users", (req, res) =>{
+app.get("/users", (req, res)=>{
+    const html = `
+    <ul>
+    ${users.map((user_email) => `<li> ${user_email.email} </li>`)}
+    </ul>`
+    ;
+    res.send(html);
+})
+app.get("/api/users", (req, res) =>{
     res.json(users);
 })
 
