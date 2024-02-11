56c559fa077b802cc4210ecd2b4c6a9e2df673b4
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-12 01:36:49 +0530

    commit

--- index.js
+++ index.js
@@ -12,14 +12,22 @@ app.get("/", (req, res)=>{
 app.get("/users", (req, res)=>{
     const html = `
     <ul>
-    ${users.map((user_email) => `<li> ${user_email.email} </li>`)}
+    ${users.map((user_email) => `<li> ${user_email.email} </li>`).join("   ")}
     </ul>`
     ;
     res.send(html);
 })
+
+//RESR API
 app.get("/api/users", (req, res) =>{
     res.json(users);
 })
+for(let i = 1; i <= 1000; i++){
+app.get(`/api/users/${i}`, (req, res)=> {
+   res.send(`Hello from ${i}`);
+    
+})
+}
 
 
 app.listen(PORT, ()=> console.log(`server listening to port ${PORT}`));
