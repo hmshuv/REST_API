e4bbc335d1da3a0514a5d59148e97fd90bbb62b7
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-12 00:36:46 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const users = require('./MOCK_DATA.json');
 const app = express();
 
 const PORT = 5050;
 
+
+//ROUTES
+app.get('/users', (req, res) =>{
+    return res.json(users);
+} );
 express().listen(PORT, ()=> console.log(`server listening to port ${PORT}`));
