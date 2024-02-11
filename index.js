98c894e363d0f86bca2352834a03f16ebddd7400
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-12 03:07:48 +0530

    commit

--- index.js
+++ index.js
@@ -7,47 +7,47 @@ const PORT = 5050;
 
 //Middleware or Pluggin
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //ROUTES
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     return res.send("Welcome to home page of REST API project");
 })
-app.get("/users", (req, res)=>{
+app.get("/users", (req, res) => {
     const html = `
     <ul>
     ${users.map((user_email) => `<li> ${user_email.email} </li>`).join("   ")}
     </ul>`
-    ;
+        ;
     res.send(html);
 })
 
 //RESR API
-app.get("/api/users", (req, res) =>{
+app.get("/api/users", (req, res) => {
     res.json(users);
 })
 // for(let i = 1; i <= 1000; i++){
 // app.get(`/api/users/${i}`, (req, res)=> {
 //    res.send(`Hello from ${i}`);
-    
+
 // })
 // }
 
 app
-.route("/api/users/:id")
-.get((req, res)=> {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-})
-.patch((req, res) => {
-        // TO DO: Edit the User with ID
-        return res.json({status: "pending"})
+    .route("/api/users/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
     })
-.delete( (req, res) => {
-            // TO DO: Delete the User with ID
-            return res.json({status: "pending"})
-        })
+    .patch((req, res) => {
+        // TO DO: Edit the User with ID
+        return res.json({ status: "pending" })
+    })
+    .delete((req, res) => {
+        // TO DO: Delete the User with ID
+        return res.json({ status: "pending" })
+    })
 // app.get("/api/users/:id", (req, res)=> {
 //     const id = Number(req.params.id);
 //     const user = users.find((user) => user.id === id);
@@ -58,8 +58,11 @@ app.post("/api/users", (req, res) => {
     // TO DO: Create New User
     const body = req.body;
     users.push(body);
-    console.log("Body", body);
-    
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        console.log("Body", body);
+    })
+
+
 })
 
 // app.patch("/api/users/:id", (req, res) => {
@@ -73,4 +76,4 @@ app.post("/api/users", (req, res) => {
 // })
 
 
-app.listen(PORT, ()=> console.log(`server listening to port ${PORT}`));
+app.listen(PORT, () => console.log(`server listening to port ${PORT}`));
