e419e3cdd0782fb2d8502821944f6958824bac82
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-12 02:00:50 +0530

    commit

--- index.js
+++ index.js
@@ -28,11 +28,42 @@ app.get("/api/users", (req, res) =>{
     
 // })
 // }
-app.get("api/users/:id", (req, res)=> {
+
+app
+.route("/api/users/:id")
+.get((req, res)=> {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     return res.json(user);
-});
+})
+.patch((req, res) => {
+        // TO DO: Edit the User with ID
+        return res.json({status: "pending"})
+    })
+.delete( (req, res) => {
+            // TO DO: Delete the User with ID
+            return res.json({status: "pending"})
+        })
+// app.get("/api/users/:id", (req, res)=> {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// });
+
+app.post("/api/users", (req, res) => {
+    // TO DO: Create New User
+    return res.json({status: "pending"})
+})
+
+// app.patch("/api/users/:id", (req, res) => {
+//     // TO DO: Edit the User with ID
+//     return res.json({status: "pending"})
+// })
+
+// app.delete("/api/users/:id", (req, res) => {
+//     // TO DO: Delete the User with ID
+//     return res.json({status: "pending"})
+// })
 
 
 app.listen(PORT, ()=> console.log(`server listening to port ${PORT}`));
