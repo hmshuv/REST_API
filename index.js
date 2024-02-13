07fb30ffdcefbee1a56113df0702004a84fbc07d
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-14 01:54:52 +0530

    commit

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post("/api/users", (req, res) => {
     const body = req.body;
     users.push({ ...body, id: users.length + 1 });
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.json({ status: "success", id: user.length + 1 });
+        return res.json({ status: "success", id: user.length });
     });
 });
 
