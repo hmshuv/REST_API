d2bc085e35b3479a731313749af14e2235a8f98b
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-15 21:07:14 +0530

    commit

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const app = express();
 const PORT = 5050;
 
 
+//Connection
+mongoose
+.connect('mongodb://127.0.0.1:27017/himanshu_first_db')
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Mongo err", err));
+
+
+
+
 // Schema 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -30,7 +39,8 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: String,
     },
-})
+}, 
+{timestamps: true})
 
 const User = mongoose.model('user', userSchema)
 
@@ -111,16 +121,22 @@ app
 //     return res.json(user);
 // });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
     // TO DO: Create New User
     const body = req.body;
     if (!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
         return res.status(400).json({ msg: "All fields are required" });
     }
-    users.push({ ...body, id: users.length + 1 });
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.status(201).json({ status: "success", id: users.length });
+    const result = await User.create({
+        firstName: body.first_name,
+        lastName:  body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title,
     });
+    
+    console.log("result", result);
+    return res.status(201).json({msg: "success"})
 });
 
 // app.patch("/api/users/:id", (req, res) => {
