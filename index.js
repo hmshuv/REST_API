cd7d32332bb4fefa00c3c670dfbab853a2ccce36
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-19 04:54:06 +0530

    i

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {connectMongoDB} = require('./connection');
+const { connectMongoDB } = require('./connection');
 const morgan = require("morgan");
 const fs = require("fs");
 const app = express();
